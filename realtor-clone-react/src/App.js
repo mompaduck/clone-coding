e7acbd4a18572b7d7d871af913530a1dc406fad4
mompaduck <mompaduck@gmail.com>
2022-10-06 22:28:31 +0900

    create the header component

--- realtor-clone-react/src/App.js
+++ realtor-clone-react/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Header from "./components/Header"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import Offers from "./pages/Offers"
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
